51ae0088d0c1f3d79e250653b157400ff8e210c2
Ryan Zemlin <rzemlin@gmail.com>
2021-02-15 16:45:52 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,7 +1,7 @@
 def hello_t(array)
   i = 0
- array.each do |names| 
-   puts "#{names.upcase}"
+ array.each do |names|
+   puts "#{names}.upcase"
   while i < array.length
     yield array[i]
     i = i + 1
